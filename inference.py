c8e23aee0725eeb3228f0bbdf6c0fa38d2779c12
jread6 <jread6@gatech.edu>
2023-10-19 16:40:28 -0400

    adding necessary extra files

---
+++ inference.py
@@ -0,0 +1,5 @@
+import sys
+import torch
+import torch.utils.data
+from torch import nn
+from torchvision import models
---
+++ test_EXAMPLE.py
@@ -0,0 +1,82 @@
+import os
+import sys
+import torch
+import torch.utils.data
+from torch import nn
+from torchvision import models
+
+from pytorch_quantization import nn as quant_nn
+
+
+from test import evaluate
+from dataset import get_imagenet
+
+from pytorch_quantization import quant_modules
+
+sys.path.append("/usr/scratch1/james/TensorRT/tools/pytorch-quantization/pytorch_quantization/cim") # change this file location to your local installation of pytorch-quantization
+from args import CIMArgs
+
+base_directory = 'example/' # set directory for accuracy results to be saved
+
+def example():
+
+    # initialize CIM simulation arguments
+    torch.cuda.set_device(1)   
+    device = 'cuda'    
+    model_name = 'ResNet50'
+    LRS=1000
+    HRS=100000 #on/off = 100
+
+    mem_values=torch.tensor([HRS, LRS], device='cuda') # set memory state values
+
+    batch_size = 100    # set batch size
+    num_iter = 5        # set number of batches to run inference on
+
+    open_rows = 128
+    adc_precision = 7 # adc precision can be reduced if open_rows is reduced
+    # set current to voltage conversion method trans-impedence amplifier (TIA) or pull-up PMOS (PU)
+    I2V = 'TIA'       
+
+    debug = False # set to true if you want additional debug messages  
+
+    # set cim arguments
+    cim_args = CIMArgs(inference=True, adc_precision=adc_precision, 
+                    open_rows=open_rows, batch_size=batch_size,
+                    mem_values=mem_values, conversion_type=I2V,
+                    debug=debug, model_name=model_name,
+                    device=device)
+
+
+    # set cim arguments for quantized layers in TensorRT
+    quant_nn.QuantConv2d.set_cim_args(cim_args)
+    quant_nn.QuantLinear.set_cim_args(cim_args)
+
+    quant_modules.initialize()
+
+    if model_name == 'ResNet18':
+        model = models.resnet18()
+    elif model_name == 'ResNet50':
+        model = models.resnet50()
+    elif model_name == 'swin_t':
+        model = models.swin_v2_t()
+
+    # some models have already been calibrated by James. 
+    # If you want to use a model not listed above, see the TensorRT github page for instructions on how to calibrate a model
+    # https://github.com/NVIDIA/TensorRT
+    model.load_state_dict(torch.load("/usr/scratch1/datasets/quant_"+model_name+"-calibrated.pth"))
+
+    model.cuda()
+
+    data_path = '/usr/scratch1/datasets/imagenet/'
+
+    # use PyTorch's get_imagenet function
+    data_loader, data_loader_test = get_imagenet(batch_size, data_path, train=True, val=True)
+
+    filename = base_directory + 'results/accuracy/TEST_NAME.csv'
+
+    criterion = nn.CrossEntropyLoss()
+    with torch.no_grad():
+        evaluate(model, criterion, data_loader_test, device="cuda", print_freq=1, num_iter=num_iter, filename=filename)
+
+if __name__ == '__main__':
+    example()
